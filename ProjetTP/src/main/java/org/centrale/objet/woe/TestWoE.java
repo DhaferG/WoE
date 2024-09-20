9f1eee912f0ff0806f917d38e8c2b5c347ecdb10
nekouki <nourkouki@localhost>
2024-09-20 14:21:17 +0200

    classe TestWoE

---
+++ ProjetTP/src/main/java/org/centrale/objet/woe/TestWoE.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.woe;
+
+/**
+ *
+ * @author nourkouki
+ */
+public class TestWoE {
+    
+    public static void main(String[] args){
+    
+    }
+    
+}
