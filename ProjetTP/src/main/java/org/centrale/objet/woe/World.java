b51c746a9ffc38d76e2f3e94ca84c5c0a95a9a5b
nekouki <nourkouki@localhost>
2024-09-20 14:21:28 +0200

    classe java

--- ProjetTP/src/main/java/org/centrale/objet/woe/World.java
+++ ProjetTP/src/main/java/org/centrale/objet/woe/World.java
@@ -3,9 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package org.centrale.objet.woe;
-import java.util.Random;
-import java.util.Set;
-import java.util.HashSet;
 
 /**
  *
@@ -19,51 +16,15 @@ public class World {
     // constructeur du monde
     
     public World() {
-    this.archer= new Archer();
-    this.paysan= new Paysan();
-    this.lapin= new Lapin();
+    this.robin= new Archer();
+    this.peon= new Paysan();
+    this.bugs= new Lapin();
     }
 
     // Méthode pour créer un monde avec des positions aléatoires pour chaque protagoniste
     public void creeMondeAlea() {
-        Random rand = new Random();
-        Set<Point2D> positions = new HashSet<>(); // Pour stocker les positions déjà utilisées
         
-        // Positionner l'archer
-        Point2D posArcher = genererPositionUnique(positions, rand);
-        archer.setPosition(posArcher);
-
-        // Positionner le paysan
-        Point2D posPaysan = genererPositionUnique(positions, rand);
-        paysan.setPosition(posPaysan);
-
-        // Positionner le lapin
-        Point2D posLapin = genererPositionUnique(positions, rand);
-        lapin.setPosition(posLapin);
     }
 
-    // Méthode pour générer une position unique (non présente dans 'positions')
-    private Point2D genererPositionUnique(Set<Point2D> positions, Random rand) {
-        Point2D pos;
-        do {
-            int x = rand.nextInt(100); // Coordonnées entre 0 et 99 (modifiable)
-            int y = rand.nextInt(100);
-            pos = new Point2D(x, y);
-        } while (positions.contains(pos)); // S'assurer que la position est unique
-        positions.add(pos); // Ajouter la nouvelle position au set
-        return pos;
-    }
-
-    // Méthode pour afficher les positions des protagonistes
-    public void afficherMonde() {
-        System.out.println("Archer position: ");
-        archer.getPosition().afficher();
-
-        System.out.println("Paysan position: ");
-        paysan.getPosition().afficher();
-
-        System.out.println("Lapin position: ");
-        lapin.getPosition().afficher();
-    }
 }
   
