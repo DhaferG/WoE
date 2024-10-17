4bc4ef2fe8b65cf9ddb62c0f3118d1fbe6a8d18a
nekouki <nourkouki@localhost>
2024-10-17 22:09:09 +0200

    implemented databasetools, added properties file to resources, added savetodatabase and getfromdatabase methods in all classes

--- ProjetTP/src/main/java/org/centrale/objet/woe/Nourriture.java
+++ ProjetTP/src/main/java/org/centrale/objet/woe/Nourriture.java
@@ -1,8 +1,20 @@
 package org.centrale.objet.woe;
 
+import java.sql.Connection;
+
 public class Nourriture extends Objet {
     public Nourriture(Point2D pos,int pts){
         super(pos, pts);
     }
+
+    @Override
+    public void saveToDatabase(Connection connection, int ID_sauvegarde, int id_inventaire, int i) {
+        
+    }
+
+    @Override
+    public void getFromDatabase(Connection connection, Integer id, int id_inventaire, String nom_objet) {
+    }
+    
 
 }
--- ProjetTP/src/main/java/org/centrale/objet/woe/NuageToxique.java
+++ ProjetTP/src/main/java/org/centrale/objet/woe/NuageToxique.java
@@ -1,8 +1,19 @@
 package org.centrale.objet.woe;
 
+import java.sql.Connection;
+
 public class NuageToxique extends Objet implements Deplacable {
     public NuageToxique(Point2D pos, int pts){
         super(pos, pts);
     }
+    @Override
     public void deplace(){}
+
+    @Override
+    public void saveToDatabase(Connection connection, int ID_sauvegarde, int id_inventaire, int i) {
+        }
+
+    @Override
+    public void getFromDatabase(Connection connection, Integer id, int id_inventaire, String nom_objet) {
+        }
 }
