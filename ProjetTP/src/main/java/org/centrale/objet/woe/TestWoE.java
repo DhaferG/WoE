875ee260af7febc2151daf1f84c97742867753c6
nekouki <nourkouki@localhost>
2024-10-21 12:42:21 +0200

    code final

--- ProjetTP/src/main/java/org/centrale/objet/woe/TestWoE.java
+++ ProjetTP/src/main/java/org/centrale/objet/woe/TestWoE.java
@@ -23,48 +23,48 @@ public class TestWoE {
     /** 
      * @param args
      */
-    // public static void main(String[] args){
-    //     DatabaseTools database = new DatabaseTools();
-    //     World monde = new World();
-    //     // Save world
-    //     database.connect();
-    //     Integer playerId = database.getPlayerID("Saegusa", "Mayumi");
-    //     database.saveWorld(playerId, "Partie2", "sauv2", monde);
-    //     database.readWorld(playerId, "Partie1", "sauv1", monde);
-    //     Point2D p = new Point2D(10,5);
-    //     // Créer un Archer avec la position initialisée
-    //     Archer archer= new Archer(100, 20, 50, 5, 10, 10, p, 10);
-    //     // creer un Paysan
-    //     Paysan paysan = new Paysan();
-    //     // creer un guerrier
-    //     Guerrier guer= new Guerrier(100, 20, 50, 5, 10, 10, p);
-    //     // sauvegarder dans la BDN
-    //     guer.saveToDatabase(database.connection, 1, 8);
-    //     archer.saveToDatabase(database.connection, 2, 2);
-    //     // charger de la BDN
-    //     guer.getFromDatabase(database.connection, 4, "guer");
-    //     archer.getFromDatabase(database.connection, 1, "archer1");
-    //     // creer un loup
-    //     Loup l = new Loup(100, 100, 50, 5, 10, p);
-    //     // enregistrer dans la bdn pour tester l'incrementation
-    //     l.saveToDatabase(database.connection, 5, 23);
-    //     l.saveToDatabase(database.connection, 5, 24);
-    //     l.saveToDatabase(database.connection, 5, 25);
-    //     l.saveToDatabase(database.connection, 5, 26);
-    //     // creer des objets, sauvegarder et les charger de la bdn
-    //     Epee ep= new Epee(p, 75, 20);
-    //     ep.saveToDatabase(database.connection, 0, 0);
-    //     ep.getFromDatabase(database.connection, 1, "epee0");
-    //     Nourriture miel=new Nourriture(p, 50,5);
-    //     miel.saveToDatabase(database.connection, 0, 0);
-    //     miel.saveToDatabase(database.connection, 0, 1);
-    //     miel.getFromDatabase(database.connection, 1, "miel0");
+    /* public static void main(String[] args){
+         DatabaseTools database = new DatabaseTools();
+         World monde = new World();
+         // Save world
+         database.connect();
+         Integer playerId = database.getPlayerID("Saegusa", "Mayumi");
+         //database.saveWorld(playerId, "Partie2", "sauv2", monde);
+         database.readWorld(playerId, "Partie1", "sauv1", monde);
+         Point2D p = new Point2D(10,5);
+         // Créer un Archer avec la position initialisée
+         Archer archer= new Archer(100, 20, 50, 5, 10, 10, p, 10);
+         // creer un Paysan
+         Paysan paysan = new Paysan();
+         // creer un guerrier
+         Guerrier guer= new Guerrier(100, 20, 50, 5, 10, 10, p);
+         // sauvegarder dans la BDN
+         guer.saveToDatabase(database.connection, 1, 50);
+         archer.saveToDatabase(database.connection, 2, 52);
+         // charger de la BDN
+         guer.getFromDatabase(database.connection, 4, "guer");
+         archer.getFromDatabase(database.connection, 1, "archer1");
+         // creer un loup
+         Loup l = new Loup(100, 100, 50, 5, 10, p);
+         // enregistrer dans la bdn pour tester l'incrementation
+         l.saveToDatabase(database.connection, 5, 80);
+         l.saveToDatabase(database.connection, 5, 24);
+         l.saveToDatabase(database.connection, 5, 25);
+         l.saveToDatabase(database.connection, 5, 26);
+         // creer des objets, sauvegarder et les charger de la bdn
+         Epee ep= new Epee(p, 75, 20);
+         ep.saveToDatabase(database.connection, 0, 100);
+         ep.getFromDatabase(database.connection, 1, "epee100");
+         Nourriture miel=new Nourriture(p, 50,5);
+         miel.saveToDatabase(database.connection, 0, 0);
+         miel.saveToDatabase(database.connection, 0, 1);
+         miel.getFromDatabase(database.connection, 1, "miel0");
                 
-    //     NuageToxique nuagetox= new NuageToxique(p, -20,5);
-    //     nuagetox.saveToDatabase(database.connection, 0, 2);
-    //     nuagetox.saveToDatabase(database.connection, 0, 1);
-    //     nuagetox.getFromDatabase(database.connection, 1, "NuageToxique1");}
-        
+         NuageToxique nuagetox= new NuageToxique(p, -20,5);
+         nuagetox.saveToDatabase(database.connection, 0, 2);
+         nuagetox.saveToDatabase(database.connection, 0, 1);
+         nuagetox.getFromDatabase(database.connection, 1, "NuageToxique1");}
+} */
     public static void main(String[] args) {
         World world = new World();
         world.creeMondeAlea(100);
@@ -85,4 +85,4 @@ public class TestWoE {
     }
 
         
-}       
+} 
